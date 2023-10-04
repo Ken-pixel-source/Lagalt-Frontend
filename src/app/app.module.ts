21fbe5d9979892ed6c78d20b0b5fad22efa41e39
kasbrudal <peephyt@gmail.com>
2023-10-04 10:07:03 +0200

    First commit
    adas

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { ProjectItemComponent } from './components/project-item/project-item.component';
+import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ProjectListComponent,
+    ProjectItemComponent,
+    ProjectPageComponent,
+    ProfilePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
