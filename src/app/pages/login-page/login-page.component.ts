fd3cf79868460c2f85bc617a97f86d562f2084de
Ken-pixel-source <84383972+Ken-pixel-source@users.noreply.github.com>
2023-10-12 08:40:50 +0200

    Feat: added keycloack frontend

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import keycloak from 'src/keycloak';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
+
+
 export class LoginPageComponent {
-  imageSrc = 'assets/images/lagaltlogo.png'
+
+  onLoginClick(){
+   keycloak.login();
+    console.log(this.onLoginClick)
+  }
 
 }
