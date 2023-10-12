4b41a63e7479f2f43d5efa8ed98bd6f6dbbbac7f
Ken-pixel-source <84383972+Ken-pixel-source@users.noreply.github.com>
2023-10-12 10:18:31 +0200

    Fixed keycloak

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import keycloak from 'src/keycloak';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-
-
 export class LoginPageComponent {
 
-  onLoginClick(){
-   keycloak.login();
-    console.log(this.onLoginClick)
-  }
+  constructor(private router: Router) {}
 
+  onLoginClick() {
+    keycloak.login()
+
+}
+
+
+  onLogoutClick() {
+    keycloak.logout()
+
+  }
 }
