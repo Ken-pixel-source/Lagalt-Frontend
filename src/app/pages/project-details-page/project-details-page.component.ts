0e76a0661ff85b8853efb9dc68fa2deab5a37ca2
Kenny <kennyhuynh95@hotmail.com>
2023-10-17 14:04:52 +0200

    keycloak from fronted

--- src/app/pages/project-details-page/project-details-page.component.ts
+++ src/app/pages/project-details-page/project-details-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/userService';
 
 @Component({
   selector: 'app-project-details-page',
   templateUrl: './project-details-page.component.html',
   styleUrls: ['./project-details-page.component.css']
 })
-export class ProjectDetailsPageComponent {
+export class ProjectDetailsPageComponent  implements OnInit{
 
+ 
+
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+
+
+    
+    this.userService.registerUser().subscribe(
+      (response) => {
+        // User registration successful, handle the response
+      },
+      (error) => {
+        // Handle errors
+        console.error('Error:', error);
+      }
+    );
+  }
 }
+
