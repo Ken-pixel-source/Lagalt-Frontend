177184bd6ff59617062704cd071ad7802732c253
kasbrudal <peephyt@gmail.com>
2023-10-18 13:46:15 +0200

    Message component and navbar styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,14 @@ import { LoginPageComponent   } from './pages/login-page/login-page.component';
 import { ProjectDetailsPageComponent } from './pages/project-details-page/project-details-page.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { NewProjectComponent } from './components/new-project/new-project.component';
+import { ProjectMemberComponent } from './components/project-member/project-member.component';
 
 const routes: Routes = [
   { path: 'project', component: ProjectPageComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: 'projects/:id', component: ProjectDetailsPageComponent },
+  {path:'project-member', component: ProjectMemberComponent},
+  {path:'project-member/:id', component: ProjectMemberComponent},
   { path: 'login', component: LoginPageComponent },
   {path: 'newproject', component: NewProjectComponent},
   { path: '', component: LandingPageComponent }, // Redirect to login page by default
