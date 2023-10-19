4bbfed940b429542ac2077185ee05ee5b0a21ab0
Kenny <kennyhuynh95@hotmail.com>
2023-10-19 20:17:44 +0200

    feat: posting skills

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -24,6 +24,8 @@ export class NavbarComponent  implements OnInit {
       return !!keycloak.authenticated;
     }
 
+    
+
 
     onLogoutClick() {
       keycloak.logout()
--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -3,19 +3,26 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/app/enviroment/enviroments';
 
 @Injectable({
-  providedIn: 'root',
-})
-export class UserService {
-  private userApiUrl = environment.projectUsers;
-
-  constructor(private readonly http: HttpClient) {}
-
-  registerUser() {
-    return this.http.post(`${this.userApiUrl}/register`, {});
+    providedIn: 'root',
+  })
+  export class UserService {
+    private userApiUrl = environment.projectUsers;
+  
+    constructor(private readonly http: HttpClient) {}
+  
+    registerUser() {
+      return this.http.post(`${this.userApiUrl}/register`, {});
+    }
+  
+    // retrieve user details by their ID
+    getUserById(userId: string) {
+      return this.http.get(`${this.userApiUrl}/${userId}`);
+    }
+    addUserSkills(userId: string, SkillName: string) {
+      return this.http.post(`${this.userApiUrl}/${userId}/skills`, { SkillName });
+    }
+    
+    
+ 
   }
-
-  // retrieve user details by their ID
-  getUserById(userId: string) {
-    return this.http.get(`${this.userApiUrl}/${userId}`);
-  }
-}
+  
