1cc43ed3e8abc272c2541b8fc87a9490b55b8637
Jacob Langevei <jacob-langevei@hotmail.com>
2023-10-24 10:54:59 +0200

    minor styling

--- src/app/enviroment/enviroments.prod.ts
+++ src/app/enviroment/enviroments.prod.ts
@@ -3,6 +3,5 @@ export const environment = {
     projectUrl: 'https://lagalt.azurewebsites.net/api/v1/projects', // Your API URL for development
     projectTypeUrl: 'https://lagalt.azurewebsites.net/api/v1/projecttypes',
     projectUsers: 'https://lagalt.azurewebsites.net/api/v1/users',
-    messageUrl: 'https://lagalt.azurewebsites.net/api/v1/projects/1/messages'
   };
   
