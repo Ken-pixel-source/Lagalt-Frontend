4faa43e7d8c53c053189a44a659b4adda37afbc4
Ken-pixel-source <84383972+Ken-pixel-source@users.noreply.github.com>
2023-10-25 22:36:38 +0200

    Feat: added remove user and from projects

--- src/app/components/project-details/project-details.component.ts
+++ src/app/components/project-details/project-details.component.ts
@@ -18,7 +18,9 @@ export class ProjectDetailsComponent implements OnInit {
   projectTagName: string | undefined;
   tags: Tags[] = [];
 
-  loading: boolean = true; 
+  loading: boolean = true;
+  hasRequestedToJoin: boolean = false;
+
 
 
   constructor(
@@ -30,28 +32,42 @@ export class ProjectDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     const projectId = this.route.snapshot.paramMap.get('id');
+
+    const userId = keycloak.tokenParsed?.sub;
+
+    if (projectId && userId) {
+      this.projectService.checkIfRequestSent(projectId, userId).subscribe({
+        next: (hasRequested) => {
+          this.hasRequestedToJoin = hasRequested;
+        },
+        error: (error) => {
+          console.error('Error checking request status:', error);
+        },
+      });
+    }
+
     if (projectId) {
       this.fetchProjectUsers(projectId);
-  
+
       this.projectService.getProjectById(projectId).subscribe({
         next: (projectData) => {
           this.project = projectData;
           if (this.project && this.project.projectTypeId) {
             this.getProjectTypeName(this.project.projectTypeId);
           }
-  
+
           // Fetch tags for the project
           this.projectService.getProjectTags(projectId).subscribe({
             next: (tagsData) => {
               this.tags = tagsData;
-  
+
               // Find project leader name
               this.userService.getUsers().subscribe((users) => {
                 const projectLeader = users.find((user) => user.userId === this.project?.ownerId);
                 if (projectLeader) {
                   this.projectLeaderName = projectLeader.userName;
                 }
-  
+
                 this.loading = false; // Only set loading to false after all data fetching is done
               });
             },
@@ -70,21 +86,28 @@ export class ProjectDetailsComponent implements OnInit {
       this.loading = false;  // Set loading to false if no projectId is available
     }
   }
-  
+
   requestToJoinProject() {
     if (this.project) {
       const projectId = this.project.projectId.toString();
       this.projectService.requestToJoinProject(projectId).subscribe({
         next: () => {
           console.log('Request to join project successful');
-          alert("request sent!")
+          this.hasRequestedToJoin = true;
+
         },
         error: (error) => {
-          console.error('Error while requesting to join the project:', error);
+          if (error && error.message && error.message.includes("User has already sent a request to join the project.")) {
+            this.hasRequestedToJoin = true;
+            alert("You've already sent a request to join this project.");
+          } else {
+            console.error('Error while requesting to join the project:', error);
+          }
         },
       });
     }
   }
+
 
   fetchProjectUsers(projectId: string) {
     this.projectService.getUsersByProjectId(parseInt(projectId, 10)).subscribe(users => {
